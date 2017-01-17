2574cfc87a868229e2c75ce658633b0880299bac
Denis Yakovenko <maximka1945@gmail.com>
2017-01-18 00:14:23 +0200

    Some fixes of HW05: Set TransactionManager logging level to debug

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,19 +2,9 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.Role;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.service.UserService;
-import ru.javawebinar.topjava.to.MealWithExceed;
-import ru.javawebinar.topjava.web.meal.MealRestController;
-import ru.javawebinar.topjava.web.user.AdminRestController;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 
 import static java.time.LocalDateTime.of;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
@@ -27,13 +17,13 @@ public class SpringMain {
     public static void main(String[] args) {
         try (GenericXmlApplicationContext appCtx = new GenericXmlApplicationContext()) {
 
-            /* Context with real DB */
+            // Context with real DB
             appCtx.getEnvironment().setActiveProfiles(Profiles.ACTIVE_DB, Profiles.DATAJPA);
             appCtx.load("spring/spring-app.xml", "spring/spring-db.xml");
-            /* Context with mock DB */
-            /*appCtx.load("spring/spring-app.xml", "spring/mock.xml");*/
+            // Context with mock DB
+            // appCtx.load("spring/spring-app.xml", "spring/mock.xml");
             appCtx.refresh();
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+            /*System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
 
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
@@ -43,24 +33,23 @@ public class SpringMain {
                     mealController.getBetween(
                             LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
                             LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);
+            filteredMealsWithExceeded.forEach(System.out::println);*/
 
-            /* Testing getWithUser method */
+            // Testing getWithUser method
             MealService mealService = appCtx.getBean(MealService.class);
-            Meal m = mealService.getWithUser(100007,100000);
-            System.out.println(m.getUser());
+            /*Meal m = mealService.getWithUser(100007,100000);
+            System.out.println(m.getUser());*/
 
-            /* Testing getWithMeals method */
-            UserService userService = appCtx.getBean(UserService.class);
+            // Testing getWithMeals method
+            /*UserService userService = appCtx.getBean(UserService.class);
             User u = userService.getWithMeals(100000);
-            System.out.println(u.getMeals());
+            System.out.println(u.getMeals());*/
 
-            System.out.println("*********************************");
-            /* Test amount of transaction when save meal */
+            //System.out.println("*********************************");
+            // Test amount of transaction when save meal
             Meal created = new Meal(999, of(2020, Month.DECEMBER, 31, 23, 59), "Новогодний Ужин", 2500);
             mealService.save(created, USER_ID);
-            System.out.println("*********************************");
-
+            //System.out.println("*********************************");
         }
     }
 }
