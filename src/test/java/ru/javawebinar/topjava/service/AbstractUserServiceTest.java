5f19a35022d51678eb304918f5cd1c59514bc362
Denis Yakovenko <maximka1945@gmail.com>
2017-01-21 18:04:14 +0200

    Implementation of HW05 (basic) Paragraphs 1.1 and 1.2 realized. All tests passing.

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,12 +1,10 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
@@ -21,6 +19,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     @Autowired
     protected UserService service;
 
+/*
     @Autowired
     protected JpaUtil jpaUtil;
 
@@ -29,6 +28,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
         service.evictCache();
         jpaUtil.clear2ndLevelHibernateCache();
     }
+*/
 
     @Test
     public void testSave() throws Exception {
